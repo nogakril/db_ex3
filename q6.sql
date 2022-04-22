13f89672fcdbb430460893d7073f4c3f2d3ffe5e
shay-tam <102288891+shay-tam@users.noreply.github.com>
2022-04-22 20:18:34 +0300

    Update q6.sql

--- q6.sql
+++ q6.sql
@@ -1,12 +1,10 @@
-with recursive t(dist, name) as (
-    select distinct 0 as dist, name
-    from authors
-    where name = 'Noam Nisan'
-    union all
-    select dist+1, S.name
-    from (t natural join authors) S, authors R  
-    where S.conference = R.conference and S.year = R.year and S.name != R.name and dist < 2
+WITH RECURSIVE t(dist, name) AS (
+    VALUES(0, 'Noam Nisan')
+    UNION
+    SELECT dist+1, R.name
+    FROM t, authors S, authors R  
+    WHERE t.name = S.name AND S.conference = R.conference AND S.year = R.year AND S.name != R.name AND dist < 2
     )
-select distinct name
-from t
-order by name;
+SELECT distinct name
+FROM t
+ORDER BY name;
