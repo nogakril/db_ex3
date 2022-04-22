1a096955f04f147b087e9723d243935d744a3c90
nogakril <81961268+nogakril@users.noreply.github.com>
2022-04-22 19:24:57 +0300

    Add files via upload

--- q6.sql
+++ q6.sql
@@ -1,11 +1,11 @@
 with recursive t(dist, name) as (
-    select values(0), name
-    from authors S
-    where S.name = 'Noam Nisan'
+    select distinct 0 as dist, name
+    from authors
+    where name = 'Noam Nisan'
     union all
-    select dist+1, name
-    from t, authors S, authors R  
-    where t.name = S.name and S.conference = R.conference and S.year = R.year and S.name != R.name and dist < 2
+    select dist+1, S.name
+    from (t natural join authors) S, authors R  
+    where S.conference = R.conference and S.year = R.year and S.name != R.name and dist < 2
     )
 select distinct name
 from t
